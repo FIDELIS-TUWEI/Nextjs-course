ca08c73a4e6f99ebd727c6abf709b4fcfc15a0ad
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-08-24 12:44:29 +0300

    Nextjs styling with Tailwindcss

--- app/components/ProductCard.tsx
+++ app/components/ProductCard.tsx
@@ -1,9 +1,21 @@
 import AddToCart from "./AddToCart";
+
+{/*
 import styles from "./ProductCard.module.css";
 
 const ProductCard = () => {
     return (
         <div className={styles.card}>
+            <AddToCart />
+        </div>
+    )
+}
+*/}  // CSS styling with module
+
+// styling with tailwindcss
+const ProductCard = () => {
+    return (
+        <div className="p-5 my-5 bg-sky-400 text-white text-xl hover:bg-sky-500">
             <AddToCart />
         </div>
     )
