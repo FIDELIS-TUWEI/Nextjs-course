67121aa51de2a67a177018fb1e0a74b618e2fb38
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-08-24 12:21:30 +0300

    exploring static & dynamic rendering methods(options)

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -7,12 +7,14 @@ interface User {
 
 const UsersPage = async () => {
   const res = await fetch("https://jsonplaceholder.typicode.com/users", 
-    { next: { revalidate: 10 } } // fetch fresh data every 10 seconds
+    //{ next: { revalidate: 10 } } // fetch fresh data every 10 seconds
+    { cache: 'no-store' } // to render this page as dynamic render
   );
   const users: User[] = await res.json();
   return (
     <>
       <h1>Users</h1>
+      <p>{new Date().toLocaleTimeString()}</p>
       <ul>
         {users.map((user) => (
           <div key={user.id}>
